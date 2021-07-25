005a035d54de280aa3fa6a196c52f0f457d5193c
Jennifer Pazos <jmp521@gmail.com>
2021-07-24 21:47:20 -0500

    add user resource

---
+++ backend/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+    has_many :drivers
+
+
+    def self.from_omniauth(auth)
+        user = User.first_or_initialize(email: auth["email"]) do |u|
+            u.username = auth["name"]
+
+        end
+        user.save
+        user
+
+    end
+
+end
+# this method doesn't need a uid
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_032302) do
+ActiveRecord::Schema.define(version: 2021_07_13_051450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_07_04_032302) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["driver_id"], name: "index_trips_on_driver_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.bigint "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
