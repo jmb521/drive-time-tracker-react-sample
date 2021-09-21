6e0624e8458f7a68b85cf5819471eb9eac90326b
Jennifer Pazos <jmp521@gmail.com>
2021-09-20 19:40:42 -0500

    added useParams hook

--- frontend/src/DriverShow.js
+++ frontend/src/DriverShow.js
@@ -2,9 +2,13 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import DriverLog from './components/drivers/DriverLog'
+import {useParams} from 'react-router-dom'
 
 const DriverShow = (props) => {
-    const driver = props.drivers.find(driver => driver.id === parseInt(props.match.params.id, 10)) 
+
+    const {id} = useParams()
+    const driver = props.drivers.find(driver => driver.id === parseInt(id, 10)) 
+    
     return(
         <div>
             {
