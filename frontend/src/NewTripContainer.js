1b7cc0e54aa15ea5c6b3f6ac9ca4c175a8a1786d
Jennifer Pazos <jmp521@gmail.com>
2021-09-20 19:28:19 -0500

    added useParams hook

--- frontend/src/NewTripContainer.js
+++ frontend/src/NewTripContainer.js
@@ -1,19 +1,22 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import NewTripForm from './NewTripForm'
-class NewTripContainer extends Component {
-    render() {
-        const driverId = parseInt(this.props.match.params.driverId, 10)
-        const findDriver = this.props.drivers.find(driver => driver.id === driverId)
-        return(
-            <div>
-                <h2>{findDriver && findDriver.first_name}</h2>
-                <NewTripForm driverId={driverId} history={this.props.history}/>
-                
-            </div>
-        )
-    }
+import {useParams} from 'react-router-dom'
+
+const NewTripContainer = (props) => {
+
+    const {driverId} = useParams()
+    console.log("params", driverId)
+    const findDriver = props.drivers.find(driver => driver.id === driverId)
+    return(
+        <div>
+            <h2>{findDriver && findDriver.first_name}</h2>
+            <NewTripForm driverId={driverId} />
+            test
+        </div>
+    )
 }
+
 const mapStateToProps = state => {
     return {
         drivers: state.driverReducer.drivers, 
